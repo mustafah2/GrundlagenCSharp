90d09f510fab12278558cdfe5762ed6ddfcae32f
Mustafa Hamko <mustafa.h11@gso.schule.koeln>
2022-03-03 11:44:56 +0100

    Projektdateien hinzufügen.

---
+++ ArrayA1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArrayA1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
