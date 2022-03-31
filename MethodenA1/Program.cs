512711b08bed5a57c2e5f8399e469502a6ebc361
Mustafa Hamko <mustafa.h11@gso.schule.koeln>
2022-03-31 11:44:41 +0200

    Methoden Aufgaben

--- MethodenA1/Program.cs
+++ MethodenA1/Program.cs
@@ -4,9 +4,56 @@ namespace MethodenA1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main()
+            
+
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Bitte geben sie ein Wort ein");
+            string n = Convert.ToString(Console.ReadLine());
+            int m=0;
+
+            bool flag;
+            do
+            {
+                flag = false;
+                Console.WriteLine("Bitte Anzhahl geben");
+                try
+                {
+                     m = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception )
+                {
+                    Console.WriteLine("Falsche Eingabe");
+                    flag = true;
+                }
+
+            } while (flag);
+            
+            int anzahl = 0;
+            anzahl = ausgabeTextMalX(n,m);
+            Console.WriteLine("/nAnzahl der Buchstaben:{0}", anzahl);
+
+
         }
+        static int ausgabeTextMalX(string text, int zahl)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                Console.WriteLine(text);
+
+                zahl = text.Length * i;
+     
+            }
+
+            return zahl;
+
+          
+
+
+
+
+        }
+        
     }
 }
